d0c403362508b8b9ce8609b139e1252c8def3a61
Penda <amelie.willems2@gmail.com>
2021-03-21 00:58:10 +0100

    add  update / delete page
    need to change the attributes like add id etc...

---
+++ assest/script/update_delete.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $('#create_select_type').change(function(){
+        //clear display
+        $("#user_attributes").css("display", "none");
+        $("#student_attributes").css("display", "none");
+        $("#delegate_attributes").css("display", "none");
+        $("#pilot_attributes").css("display", "none");
+        $("#admin_attributes").css("display", "none");
+        $("#compagny_attributes").css("display", "none");
+        $("#internship_attributes").css("display", "none");
+
+        //display attributes of the choosen category
+        if($('#create_select_type option:selected').text()=="Student"){
+            $("#user_attributes").css("display", "block");
+            $("#student_attributes").css("display", "block");
+        }
+        if($('#create_select_type option:selected').text()=="Delegate"){
+            $("#user_attributes").css("display", "block");
+            $("#delegate_attributes").css("display", "block");
+        }
+        if($('#create_select_type option:selected').text()=="Pilot"){
+            $("#user_attributes").css("display", "block");
+            $("#pilot_attributes").css("display", "block");
+        }
+        if($('#create_select_type option:selected').text()=="Admin"){
+            $("#user_attributes").css("display", "block");
+            $("#admin_attributes").css("display", "block");
+        }
+        if($('#create_select_type option:selected').text()=="Internship"){
+            $("#internship_attributes").css("display", "block");
+        }
+        if($('#create_select_type option:selected').text()=="Compagny"){
+            $("#compagny_attributes").css("display", "block");
+        }
+        
+        
+
+    
+        
+    });
+    
+
+
+
+
+
+
+
+});
