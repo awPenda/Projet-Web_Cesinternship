a8ef386478dce3449439b61bd9276820f786783e
Penda <amelie.willems2@gmail.com>
2021-03-23 23:41:29 +0100

    Update header.php

--- assest/pages/header.php
+++ assest/pages/header.php
@@ -1,9 +1,19 @@
 <header>
         <nav class="navbar navbar-dark bg-dark">
-            <h1 id="Title_cesinternship"><img src="../pictures/CesinternshipLOGO.png" alt="Cesinternship" width="250"></h1>
-            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+            
+                <h1 id="Title_cesinternship"><img src="../pictures/CesinternshipLOGO.png" alt="Cesinternship" width="250"></h1>
+                <div class="">
+                    <button class="navbar-toggler collapsed"><i class="fas fa-home"></i></button>
+
+                    <button class="navbar-toggler collapsed"><i class="fas fa-plus"></i></button>
+
+                    <button class="navbar-toggler collapsed"><i class="fas fa-edit"></i></button>
+                    
+                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            
         
             <div class="navbar-collapse collapse" id="navbarsExample01" >
                 <p class="text-center" style="color: white;">
