ce14cc15a5729fdf2a0b9a603ebbf89b912dddb7
Penda <amelie.willems2@gmail.com>
2021-03-26 14:23:06 +0100

    add js to home page

--- assest/script/home.js
+++ assest/script/home.js
@@ -1,31 +1,33 @@
 $(document).ready(function(){
 
+    window.alert("hey!");
+
     $('#search_select_type').change(function(){
+
+        window.alert("hey!");
+
         //clear display
-        $("#user_attributes").css("display", "none");
+        $("#search_compagny_div").css("display", "none");
+        $("#search_internship_div").css("display", "none");
+        $("#search_pilot_div").css("display", "none");
+        $("#search_delegate_div").css("display", "none");
+        $("#search_student_div").css("display", "none");
 
         //display attributes of the choosen category
-        if($('#create_select_type option:selected').text()=="Student"){
-            $("#user_attributes").css("display", "block");
-            $("#student_attributes").css("display", "block");
+        if($('#search_select_type option:selected').text()=="Student"){
+            $("#search_student_div").css("display", "block");
         }
-        if($('#create_select_type option:selected').text()=="Delegate"){
-            $("#user_attributes").css("display", "block");
-            $("#delegate_attributes").css("display", "block");
+        if($('#search_select_type option:selected').text()=="Delegate"){
+            $("#search_delegate_div").css("display", "block");
         }
-        if($('#create_select_type option:selected').text()=="Pilot"){
-            $("#user_attributes").css("display", "block");
-            $("#pilot_attributes").css("display", "block");
+        if($('#search_select_type option:selected').text()=="Pilot"){
+            $("#search_pilot_div").css("display", "block");
         }
-        if($('#create_select_type option:selected').text()=="Admin"){
-            $("#user_attributes").css("display", "block");
-            $("#admin_attributes").css("display", "block");
+        if($('#search_select_type option:selected').text()=="Internship"){
+            $("#search_internship_div").css("display", "block");
         }
-        if($('#create_select_type option:selected').text()=="Internship"){
-            $("#internship_attributes").css("display", "block");
-        }
-        if($('#create_select_type option:selected').text()=="Compagny"){
-            $("#compagny_attributes").css("display", "block");
+        if($('#search_select_type option:selected').text()=="Compagny"){
+            $("#search_compagny_div").css("display", "block");
         }
         
         
