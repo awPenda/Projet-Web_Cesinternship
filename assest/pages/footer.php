64b595f411f36153572a08829d04dd891244929d
Penda <amelie.willems2@gmail.com>
2021-03-27 00:22:47 +0100

    juste mise en page du code

--- assest/pages/footer.php
+++ assest/pages/footer.php
@@ -1,8 +1,8 @@
 <footer class="my-5 text-muted text-center text-small">
-            <p class="mb-1">© 2021 Cesinternship</p>
-            <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Privacy</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Support</a></li>
-            </ul>
-        </footer>
+    <p class="mb-1">© 2021 Cesinternship</p>
+    <ul class="list-inline">
+        <li class="list-inline-item"><a href="#">Privacy</a></li>
+        <li class="list-inline-item"><a href="#">Terms</a></li>
+        <li class="list-inline-item"><a href="#">Support</a></li>
+    </ul>
+</footer>
--- assest/pages/header.php
+++ assest/pages/header.php
@@ -1,23 +1,21 @@
 <header>
-        <nav class="navbar navbar-dark bg-dark">
-            
-                <h1 id="Title_cesinternship"><img src="../pictures/CesinternshipLOGO.png" alt="Cesinternship" width="250"></h1>
-                <div class="">
-                    <!-- retirer et le faire propre dans le js ou un truc comme ça -->
-                    <a href="../pages/home.php"><button class="navbar-toggler collapsed"><i class="fas fa-home"></i></button></a>
-                    <a href="../pages/create.php"><button class="navbar-toggler collapsed"><i class="fas fa-plus"></i></button></a>
-                    <a href="../pages/update_delete.php"><button class="navbar-toggler collapsed"><i class="fas fa-edit"></i></button></a>
+    <nav class="navbar navbar-dark bg-dark"> 
+        <h1 id="Title_cesinternship"><img src="../pictures/CesinternshipLOGO.png" alt="Cesinternship" width="250"></h1>
+        <div class="">
+            <!-- retirer et le faire propre dans le js ou un truc comme ça, attention un bouton dans un a ça se fait pas-->
+            <a href="../pages/home.php"><button class="navbar-toggler collapsed"><i class="fas fa-home"></i></button></a>
+            <a href="../pages/create.php"><button class="navbar-toggler collapsed"><i class="fas fa-plus"></i></button></a>
+            <a href="../pages/update_delete.php"><button class="navbar-toggler collapsed"><i class="fas fa-edit"></i></button></a>
 
-                    <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
             
-        
-            <div class="navbar-collapse collapse" id="navbarsExample01" >
-                <p class="text-center" style="color: white;">
+        <div class="navbar-collapse collapse" id="navbarsExample01" >
+            <p class="text-center" style="color: white;">
                 Find your intership with Cesinternship !
-                </p>
-            </div>
-        </nav>
-    </header>
+            </p>
+        </div>
+    </nav>
+</header>
