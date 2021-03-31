3c49f935e60e9970d6f69da33576f2f6eb22ce3d
Penda <amelie.willems2@gmail.com>
2021-04-01 01:41:29 +0200

    fuxck

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-include "./assest/php/classes/DB_class.php";
-include "./assest/php/classes/connection.php";
+include "./assets/php/classes/DB_class.php";
+include "./assets/php/classes/connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,18 +11,18 @@ include "./assest/php/classes/connection.php";
      <meta name="description" content="Cesinternship Sign-in page">
      <meta name="author" content="Amélie WILLEMS">
      <!-- intégration de la feuille CSS de bootstrap -->
-     <link rel="stylesheet" href="./assest/vendors/bootstrap/bootstrap-4.5.3-dist/bootstrap-4.5.3-dist/css/bootstrap.min.css">
+     <link rel="stylesheet" href="./assets/vendors/bootstrap/bootstrap-4.5.3-dist/bootstrap-4.5.3-dist/css/bootstrap.min.css">
      <!-- Ajout de la librairie pour les icones -->
-     <link rel="stylesheet" href="./assest/vendors/fontawesome/fontawesome-free-5.15.1-web/css/all.min.css">
+     <link rel="stylesheet" href="./assets/vendors/fontawesome/fontawesome-free-5.15.1-web/css/all.min.css">
      <!-- Intégration du script jquery -->
-     <script src="./assest/vendors/jquery/jquery-3.5.1.min.js"></script>
+     <script src="./assets/vendors/jquery/jquery-3.5.1.min.js"></script>
      <!-- Intégration du script de bootstrap, il doit impérativement être après jquery et popper.js-->
-     <script src="./assest/vendors/bootstrap/bootstrap-4.5.3-dist/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js" ></script>
+     <script src="./assets/vendors/bootstrap/bootstrap-4.5.3-dist/bootstrap-4.5.3-dist/js/bootstrap.bundle.min.js" ></script>
      <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
      <!-- Lien page de style -->
-     <link rel="stylesheet" href="./assest/style/style.css">
+     <link rel="stylesheet" href="./assets/style/style.css">
      <!-- Titre de la page-->
      <title>Connexion</title>
 </head>
@@ -30,7 +30,7 @@ include "./assest/php/classes/connection.php";
 <body class="raw">
   
     <nav class="navbar navbar-dark bg-dark">
-        <h1 id="Title_cesinternship"><img src="./assest/pictures/Cesinternship.png" alt="Cesinternship" width="250"></h1>
+        <h1 id="Title_cesinternship"><img src="./assets/pictures/Cesinternship.png" alt="Cesinternship" width="250"></h1>
         <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -45,7 +45,7 @@ include "./assest/php/classes/connection.php";
 
     <div id="main_div" class="text-center">
         <form class="form-signin" method="POST">
-            <img class="mb-4" src="./assest/pictures/LOGO.png" alt="LOGO" style="width:50%">
+            <img class="mb-4" src="./assets/pictures/LOGO.png" alt="LOGO" style="width:50%">
             
             <h2 class="h3 mb-3 font-weight-normal">Please sign in</h2>
   
@@ -60,7 +60,7 @@ include "./assest/php/classes/connection.php";
             <!-- <label for="id_user_type" class="sr-only">Email address</label> -->
             <input type="text" id="user_name" name="user_name" class="form-control" placeholder="Username" required="">
             <!-- <label for="input_password" class="sr-only">Password</label> -->
-            <input type="password" id="input_password" name="input_password" class="form-control" placeholder="Password" required="">
+            <input type="password" id="input_password" name="input_password" class="form-control" placeholder="Password" required="" autocomplete>
             
             <button id="sign_in_btn" name="sign_in_btn" class="btn btn-lg btn-block" type="submit">Sign in</button>
         </form>
@@ -125,6 +125,20 @@ include "./assest/php/classes/connection.php";
           <li class="list-inline-item"><a href="#">Support</a></li>
         </ul>
       </footer>
+
+    <script>
+      if('serviceWorker' in navigator){
+        window.addEventListener('load', () => {
+          navigator.serviceWorker.register('/sw.js')
+          .then(reg => {
+            console.log('Service Worker registered !');
+          })
+          .catch(err => {
+            console.log('Service Worker registration failed :', err);
+          });
+        });
+      }
+    </script>
 
     
 </body>
---
+++ sw.js
@@ -0,0 +1,55 @@
+console.log('Service worker wake up...');
+
+self.addEventListener('install', event => {
+    console.log('Service worker is  installed !');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    console.log('Service worker cache is ready !');
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service worker is activated !');
+});
+
+const CACHE_NAME = 'Csintship_cache_v1';
+const urlsToCache = [
+    './',
+    './index.php',
+
+    //  './assets/pages/create.php',
+    //  './assets/pages/home.php',
+    //  './assets/pages/update_delete.php',
+
+     './assets/php/classes/compagny.php',
+     './assets/php/classes/connection.php',
+     './assets/php/classes/DB_class.php',
+     './assets/php/classes/footer.php',
+     './assets/php/classes/header.php',
+     './assets/php/classes/internship.php',
+     './assets/php/classes/user.php',
+
+     './assets/php/template/locality.class.php',
+     './assets/php/template/test.php',
+
+     './assets/pictures/Cesinternship.png',
+     './assets/pictures/cesinternshipLOGO.png',
+     './assets/pictures/CesintershipLOGO.png',
+     './assets/pictures/LOGO.png',
+
+     './assets/script/create.js',
+     './assets/script/home.js',
+     './assets/script/update_delete.js',
+
+     './assets/style/home_style.css',
+     './assets/style/style.css',
+
+      './test_connection\\hanton/connexion.php',
+    //   './test_connection\\hanton/deconnexion.php',
+    //   './test_connection\\hanton/Page_test.php',
+      './test_connection\\hanton/Sign-in.php',
+    // './test_connection\\hanton/',
+]
